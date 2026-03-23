a8e0888683f1a18d063991c137feb39ab22140cd
indahcs <sanclar8c@gmail.com>
2026-03-23 09:28:36 -0400

    network modules and gitignore

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,86 +1,100 @@
-data "aws_availability_zones" "available" {
-  state = "available"
-}
+# data "aws_availability_zones" "available" {
+#   state = "available"
+# }
 
-locals {
-  default_tags = merge(
-    var.default_tags, {"Env" = var.env}
-  )
-}
+# locals {
+#   default_tags = merge(
+#     var.default_tags, {"Env" = var.env}
+#   )
+# }
 
+# --- VPC ---
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
-  tags       = merge(local.default_tags, {"Name" = "${var.prefix}-vpc"})
+  tags       = {"Name" = "${var.env}-vpc"}
 }
 
+# --- Public Subnets ---
 resource "aws_subnet" "public_subnet" {
   count                   = length(var.public_subnet_cidrs)
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidrs[count.index]
-  availability_zone       = data.aws_availability_zones.available.names[count.index % length(data.aws_availability_zones.available.names)]
+  availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = true
-  tags                    = merge(local.default_tags, {"Name" = "${var.prefix}-public-subnet-${count.index + 1}"})
+  tags                    = {"Name" = "${var.env}-public-subnet-${count.index + 1}"}
 }
 
+# --- Private Subnets ---
 resource "aws_subnet" "private_subnet" {
   count                   = length(var.private_subnet_cidrs)
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.private_subnet_cidrs[count.index]
-  availability_zone       = data.aws_availability_zones.available.names[count.index % length(data.aws_availability_zones.available.names)]
+  availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = false
-  tags                    = merge(local.default_tags, {"Name" = "${var.prefix}-private-subnet-${count.index + 1}"})
+  tags                    = {"Name" = "${var.env}-private-subnet-${count.index + 1}"}
 }
 
+# --- Internet Gateway (only if enable_igw = true) ---
 resource "aws_internet_gateway" "igw" {
-  count = length(var.public_subnet_cidrs) > 0 ? 1 : 0
+  count = var.enable_igw && length(var.public_subnet_cidrs) > 0 ? 1 : 0
   vpc_id = aws_vpc.main.id 
-  tags = merge(local.default_tags, {"Name" = "${var.prefix}-igw"})
+  tags = {"Name" = "${var.env}-igw"}
 }
 
+# --- Elastic IP for NAT Gateway ---
 resource "aws_eip" "nat_eip" {
-  count = var.enable_nat ? 1 : 0
+  count = var.enable_nat ? 1 : 0 # Only create an Elastic IP if NAT Gateway is enabled
+  domain = "vpc"
+  tags = {"Name" = "${var.env}-nat-eip"}
 }
 
+# --- NAT Gateway (only if enable_nat_gateway = true) ---
 resource "aws_nat_gateway" "nat_gw" {
   count         = var.enable_nat ? 1 : 0
-  allocation_id = aws_eip.nat_eip[0].id
+  allocation_id = aws_eip.nat_eip[0].id #NAT GW needs an Elastic IP (a fixed public IP address) attached to it
   subnet_id     = aws_subnet.public_subnet[0].id
-  tags          = merge(local.default_tags, {"Name" = "${var.prefix}-nat-gw-${count.index + 1}"})
+  tags          = {"Name" = "${var.env}-nat-gw-${count.index + 1}"}
+
+  depends_on = [aws_internet_gateway.igw] # Ensure IGW is created before NAT GW since NAT GW needs to be in a public subnet which requires IGW for internet access
 }
 
+# --- Public Route Table (routes internet traffic through IGW) ---
 resource "aws_route_table" "public_rt" {
-  count = length(var.public_subnet_cidrs) > 0 ? 1 : 0
+  count = var.enable_igw && length(var.public_subnet_cidrs) > 0 ? 1 : 0
   vpc_id = aws_vpc.main.id
-  tags   = merge(local.default_tags, {"Name" = "${var.prefix}-public-rt"})
-}
 
-resource "aws_route" "public_route" {
-  count = length(var.public_subnet_cidrs) > 0 ? 1 : 0
-  route_table_id         = aws_route_table.public_rt[0].id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw[0].id
-}
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw[0].id
+  }
 
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.main.id
-  tags   = merge(local.default_tags, {"Name" = "${var.prefix}-private-rt"})
-}
-
-resource "aws_route" "private_route" {
-  count = var.enable_nat ? 1 : 0 
-  route_table_id = aws_route_table.private_rt.id 
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.nat_gw[0].id
+  tags   = {"Name" = "${var.env}-public-rt"}
 }
 
 resource "aws_route_table_association" "public_routes" {
   count = length(aws_subnet.public_subnet)
-  route_table_id = aws_route_table.public_rt[0].id
   subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_rt[0].id
+}
+
+# --- Private Route Table (routes internet traffic through NAT GW) ---
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  dynamic "route" { #the NAT route only appears if NAT GW exists, 
+  # prod's private route table will have no outbound internet route at all, which is correct since prod has no public subnets.
+    for_each = var.enable_nat ? [1] : []
+    content {
+      cidr_block     = "0.0.0.0/0"
+      nat_gateway_id = aws_nat_gateway.nat_gw[0].id
+    }
+  }
+
+  tags   = {"Name" = "${var.env}-private-rt"}
 }
 
 resource "aws_route_table_association" "private_routes" {
   count = length(aws_subnet.private_subnet)
-  route_table_id = aws_route_table.private_rt[0].id
   subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.private_rt.id
 }
--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,6 +1,10 @@
-variable "default_tags" {
-  type        = map(any)
-  description = "Default tags to be applied to all AWS resources"
+# variable "default_tags" {
+#   type        = map(any)
+#   description = "Default tags to be applied to all AWS resources"
+# }
+variable "availability_zones" {
+  description = "List of availability zones to deploy subnets into"
+  type        = list(string)
 }
 variable "vpc_cidr" {
     type = string
@@ -14,15 +18,21 @@ variable "private_subnet_cidrs" {
     type = list(string)
     description = "Private Subnet CIDRs"
 }
+variable "enable_igw" {
+  type = bool
+  description = "Whether to create Internet Gateway for public subnets"
+  default     = false
+}
 variable "enable_nat" {
   type = bool
   description = "Whether to create NAT Gateway for private subnets"
+  default     = false
 }
 variable "env" {
     type = string
     description = "Environment name"
 }
-variable "prefix" {
-  type        = string
-  description = "Naming prefix"
-}
+# variable "prefix" {
+#   type        = string
+#   description = "Naming prefix"
+# }
